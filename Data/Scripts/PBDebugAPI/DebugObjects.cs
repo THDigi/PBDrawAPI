ca3a3cbd830367008b1c3c21029a78ff589d51ec
Digi <844339+THDigi@users.noreply.github.com>
2023-11-26 09:15:18 +0200

    Improved on-top draw
    - Changed from blendtype additive to drawing the same billboards twice, once in 3D and again very close to camera with reduced color intensity, to be able to easily discern if it's actually in view or behind some geometry.
    - Rewrote sphere render to be able to use it really close to camera and have some optimizations too.

--- Data/Scripts/PBDebugAPI/DebugObjects.cs
+++ Data/Scripts/PBDebugAPI/DebugObjects.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Sandbox.Definitions;
 using Sandbox.ModAPI;
 using VRage.Collections;
@@ -24,10 +25,35 @@ namespace Digi.PBDebugAPI
 
     public abstract class DebugDrawBillboardBase : DebugObjectBase, IDrawObject
     {
-        protected BlendTypeEnum BlendType;
+        protected bool DrawOnTop;
+
+        protected static readonly BlendTypeEnum BlendType = BlendTypeEnum.PostPP;
 
         protected static readonly MyStringId MaterialSquare = MyStringId.GetOrCompute("Square");
         protected static readonly MyStringId MaterialDot = MyStringId.GetOrCompute("WhiteDot");
+
+        protected const float OnTopColorMul = 0.5f;
+
+        const float DepthRatioF = 0.01f;
+
+        protected static float ToAlwaysOnTop(ref MatrixD matrix)
+        {
+            MatrixD camMatrix = MyAPIGateway.Session.Camera.WorldMatrix;
+            Vector3D posOverlay = camMatrix.Translation + ((matrix.Translation - camMatrix.Translation) * DepthRatioF);
+
+            MatrixD.Rescale(ref matrix, DepthRatioF);
+            matrix.Translation = posOverlay;
+
+            return DepthRatioF;
+        }
+
+        protected static float ToAlwaysOnTop(ref Vector3D position)
+        {
+            MatrixD camMatrix = MyAPIGateway.Session.Camera.WorldMatrix;
+            position = camMatrix.Translation + ((position - camMatrix.Translation) * DepthRatioF);
+
+            return DepthRatioF;
+        }
     }
 
     public class DrawPoint : DebugDrawBillboardBase
@@ -39,7 +65,7 @@ namespace Digi.PBDebugAPI
 
         public void Init(MyConcurrentPool<DrawPoint> pool, Vector3D origin, Color color, float radius, float seconds, bool onTop)
         {
-            BlendType = (onTop ? BlendTypeEnum.AdditiveTop : BlendTypeEnum.Standard);
+            DrawOnTop = onTop;
             LiveSeconds = seconds;
             Pool = pool;
 
@@ -50,7 +76,17 @@ namespace Digi.PBDebugAPI
 
         public override void Update()
         {
-            MyTransparentGeometry.AddPointBillboard(MaterialDot, Color, Origin, Radius, 0, blendType: BlendType);
+            Color color = Color;
+            Vector3D pos = Origin;
+            float radius = Radius;
+            MyTransparentGeometry.AddPointBillboard(MaterialDot, Color, pos, radius, 0, blendType: BlendType);
+
+            if(DrawOnTop)
+            {
+                float depthScale = ToAlwaysOnTop(ref pos);
+                radius *= depthScale;
+                MyTransparentGeometry.AddPointBillboard(MaterialDot, Color * OnTopColorMul, pos, radius, 0, blendType: BlendType);
+            }
         }
 
         public override void Dispose()
@@ -70,7 +106,7 @@ namespace Digi.PBDebugAPI
 
         public void Init(MyConcurrentPool<DrawLine> pool, Vector3D from, Vector3D to, Color color, float thickness, float seconds, bool onTop)
         {
-            BlendType = (onTop ? BlendTypeEnum.AdditiveTop : BlendTypeEnum.Standard);
+            DrawOnTop = onTop;
             LiveSeconds = seconds;
             Pool = pool;
 
@@ -82,7 +118,18 @@ namespace Digi.PBDebugAPI
 
         public override void Update()
         {
-            MyTransparentGeometry.AddLineBillboard(MaterialSquare, Color, From, Direction, 1f, Thickness, blendType: BlendType);
+            Vector3D from = From;
+            Vector3 dir = Direction;
+            float thick = Thickness;
+            MyTransparentGeometry.AddLineBillboard(MaterialSquare, Color, from, dir, 1f, thick, blendType: BlendType);
+
+            if(DrawOnTop)
+            {
+                float depthScale = ToAlwaysOnTop(ref from);
+                dir *= depthScale;
+                thick *= depthScale;
+                MyTransparentGeometry.AddLineBillboard(MaterialSquare, Color * OnTopColorMul, from, dir, 1f, thick, blendType: BlendType);
+            }
         }
 
         public override void Dispose()
@@ -103,7 +150,7 @@ namespace Digi.PBDebugAPI
 
         public void Init(MyConcurrentPool<DrawAABB> pool, BoundingBoxD bb, Color color, int style, float thickness, float seconds, bool onTop)
         {
-            BlendType = (onTop ? BlendTypeEnum.AdditiveTop : BlendTypeEnum.Standard);
+            DrawOnTop = onTop;
             LiveSeconds = seconds;
             Pool = pool;
 
@@ -116,7 +163,17 @@ namespace Digi.PBDebugAPI
 
         public override void Update()
         {
-            MySimpleObjectDraw.DrawTransparentBox(ref WorldMatrix, ref LocalBox, ref Color, Style, 1, Thickness, MaterialSquare, MaterialSquare, blendType: BlendType);
+            MatrixD wm = WorldMatrix;
+            float thick = Thickness;
+            MySimpleObjectDraw.DrawTransparentBox(ref wm, ref LocalBox, ref Color, Style, 1, thick, MaterialSquare, MaterialSquare, blendType: BlendType);
+
+            if(DrawOnTop)
+            {
+                Color color = Color * OnTopColorMul;
+                float depthScale = ToAlwaysOnTop(ref wm);
+                thick *= depthScale;
+                MySimpleObjectDraw.DrawTransparentBox(ref wm, ref LocalBox, ref color, Style, 1, thick, MaterialSquare, MaterialSquare, blendType: BlendType);
+            }
         }
 
         public override void Dispose()
@@ -137,7 +194,7 @@ namespace Digi.PBDebugAPI
 
         public void Init(MyConcurrentPool<DrawOBB> pool, MyOrientedBoundingBoxD obb, Color color, int style, float thickness, float seconds, bool onTop)
         {
-            BlendType = (onTop ? BlendTypeEnum.AdditiveTop : BlendTypeEnum.Standard);
+            DrawOnTop = onTop;
             LiveSeconds = seconds;
             Pool = pool;
 
@@ -151,7 +208,17 @@ namespace Digi.PBDebugAPI
 
         public override void Update()
         {
-            MySimpleObjectDraw.DrawTransparentBox(ref WorldMatrix, ref LocalBox, ref Color, Style, 1, Thickness, MaterialSquare, MaterialSquare, blendType: BlendType);
+            MatrixD wm = WorldMatrix;
+            float thick = Thickness;
+            MySimpleObjectDraw.DrawTransparentBox(ref wm, ref LocalBox, ref Color, Style, 1, thick, MaterialSquare, MaterialSquare, blendType: BlendType);
+
+            if(DrawOnTop)
+            {
+                Color color = Color * OnTopColorMul;
+                float depthScale = ToAlwaysOnTop(ref wm);
+                thick *= depthScale;
+                MySimpleObjectDraw.DrawTransparentBox(ref wm, ref LocalBox, ref color, Style, 1, thick, MaterialSquare, MaterialSquare, blendType: BlendType);
+            }
         }
 
         public override void Dispose()
@@ -163,7 +230,7 @@ namespace Digi.PBDebugAPI
 
     public class DrawSphere : DebugDrawBillboardBase
     {
-        MatrixD Matrix;
+        MatrixD WorldMatrix;
         Color Color;
         float Radius;
         float Thickness;
@@ -171,23 +238,143 @@ namespace Digi.PBDebugAPI
         int WireDivide;
         MyConcurrentPool<DrawSphere> Pool;
 
+        const int LinesPerDegreeLimit = 10;
+
+        static List<Vector3> TempVertices = new List<Vector3>();
+
         public void Init(MyConcurrentPool<DrawSphere> pool, BoundingSphereD sphere, Color color, int style, float thickness, float lineEveryDegrees, float seconds, bool onTop)
         {
-            BlendType = (onTop ? BlendTypeEnum.AdditiveTop : BlendTypeEnum.Standard);
+            DrawOnTop = onTop;
             LiveSeconds = seconds;
             Pool = pool;
 
-            Matrix = MatrixD.CreateTranslation(sphere.Center);
+            WorldMatrix = MatrixD.CreateTranslation(sphere.Center);
             Radius = (float)sphere.Radius;
             Color = color;
             Thickness = thickness;
             Style = (MySimpleObjectRasterizer)style;
-            WireDivide = (int)MathHelper.Clamp(360 / lineEveryDegrees, 1, 360 * 10); // capped to 10 lines per degree
+            WireDivide = (int)MathHelper.Clamp(360 / lineEveryDegrees, 1, 360 * LinesPerDegreeLimit);
         }
 
         public override void Update()
         {
-            MySimpleObjectDraw.DrawTransparentSphere(ref Matrix, Radius, ref Color, Style, WireDivide, MaterialSquare, MaterialSquare, Thickness, blendType: BlendType);
+            MatrixD wm = WorldMatrix;
+            float radius = Radius;
+            float thick = Thickness;
+
+            TempVertices.Clear();
+            GenerateHalfSphereLocal(WireDivide, TempVertices);
+
+            RenderSphere(TempVertices, wm, radius, Color, thick, Style);
+
+            if(DrawOnTop)
+            {
+                float depthScale = ToAlwaysOnTop(ref wm);
+                thick *= depthScale;
+                // radius does not need scaling because worldmatrix is already
+                RenderSphere(TempVertices, wm, radius, Color * OnTopColorMul, thick, Style);
+            }
+        }
+
+        static void RenderSphere(List<Vector3> localVerts, MatrixD wm, float radius, Color color, float thick, MySimpleObjectRasterizer style)
+        {
+            // HACK: cloned DrawTransparentSphere() to remove the length > 0.1 check on lines, making it unable to work with DrawOnTop
+
+            Vector3D vctPos = Vector3D.Zero;
+            MyQuadD quad;
+
+            for(int i = 0; i < 2; i++) // two halves
+            {
+                bool flip = i != 0;
+
+                for(int v = 0; v < localVerts.Count; v += 4)
+                {
+                    Vector3 v0 = localVerts[v + 1] * radius;
+                    Vector3 v1 = localVerts[v + 3] * radius;
+                    Vector3 v2 = localVerts[v + 2] * radius;
+                    Vector3 v3 = localVerts[v] * radius;
+
+                    if(flip)
+                    {
+                        v0.Y = -v0.Y;
+                        v1.Y = -v1.Y;
+                        v2.Y = -v2.Y;
+                        v3.Y = -v3.Y;
+                    }
+
+                    quad.Point0 = Vector3D.Transform(v0, wm);
+                    quad.Point1 = Vector3D.Transform(v1, wm);
+                    quad.Point2 = Vector3D.Transform(v2, wm);
+                    quad.Point3 = Vector3D.Transform(v3, wm);
+
+                    if(style == MySimpleObjectRasterizer.Solid || style == MySimpleObjectRasterizer.SolidAndWireframe)
+                    {
+                        MyTransparentGeometry.AddQuad(MaterialSquare, ref quad, color, ref vctPos, blendType: BlendType);
+                    }
+
+                    if(style == MySimpleObjectRasterizer.Wireframe || style == MySimpleObjectRasterizer.SolidAndWireframe)
+                    {
+                        {
+                            Vector3D point = quad.Point0;
+                            Vector3 dir = quad.Point1 - point;
+                            MyTransparentGeometry.AddLineBillboard(MaterialSquare, color, point, dir, 1f, thick, blendType: BlendType);
+                        }
+                        {
+                            Vector3D point = quad.Point1;
+                            Vector3 dir = quad.Point2 - point;
+                            MyTransparentGeometry.AddLineBillboard(MaterialSquare, color, point, dir, 1f, thick, blendType: BlendType);
+                        }
+                    }
+                }
+            }
+        }
+
+        static void GenerateHalfSphereLocal(int steps, List<Vector3> vertices)
+        {
+            vertices.Clear();
+
+            double angleStep = MathHelperD.ToRadians(360d / steps);
+            double ang1max = MathHelperD.PiOver2 - angleStep;
+            double ang2max = MathHelperD.TwoPi - angleStep;
+            Vector3D vec;
+
+            for(double ang1 = 0d; ang1 <= ang1max; ang1 += angleStep)
+            {
+                double ang1sin = Math.Sin(ang1);
+                double ang1cos = Math.Cos(ang1);
+
+                for(double ang2 = 0d; ang2 <= ang2max; ang2 += angleStep)
+                {
+                    double ang2sin = Math.Sin(ang2);
+                    double ang2cos = Math.Cos(ang2);
+
+                    double nextAng1sin = Math.Sin(ang1 + angleStep);
+                    double nextAng1cos = Math.Cos(ang1 + angleStep);
+
+                    double nextAng2sin = Math.Sin(ang2 + angleStep);
+                    double nextAng2cos = Math.Cos(ang2 + angleStep);
+
+                    vec.X = ang2sin * ang1sin;
+                    vec.Y = ang1cos;
+                    vec.Z = ang2cos * ang1sin;
+                    vertices.Add(vec);
+
+                    vec.X = ang2sin * nextAng1sin;
+                    vec.Y = nextAng1cos;
+                    vec.Z = ang2cos * nextAng1sin;
+                    vertices.Add(vec);
+
+                    vec.X = nextAng2sin * ang1sin;
+                    vec.Y = ang1cos;
+                    vec.Z = nextAng2cos * ang1sin;
+                    vertices.Add(vec);
+
+                    vec.X = nextAng2sin * nextAng1sin;
+                    vec.Y = nextAng1cos;
+                    vec.Z = nextAng2cos * nextAng1sin;
+                    vertices.Add(vec);
+                }
+            }
         }
 
         public override void Dispose()
@@ -206,7 +393,7 @@ namespace Digi.PBDebugAPI
 
         public void Init(MyConcurrentPool<DrawMatrix> pool, MatrixD matrix, float length, float thickness, float seconds, bool onTop)
         {
-            BlendType = (onTop ? BlendTypeEnum.AdditiveTop : BlendTypeEnum.Standard);
+            DrawOnTop = onTop;
             LiveSeconds = seconds;
             Pool = pool;
 
@@ -217,14 +404,30 @@ namespace Digi.PBDebugAPI
 
         public override void Update()
         {
-            MyTransparentGeometry.AddLineBillboard(MaterialSquare, Color.Red, Matrix.Translation, Matrix.Right, Length, Thickness, blendType: BlendType);
-            MyTransparentGeometry.AddLineBillboard(MaterialSquare, Color.Green, Matrix.Translation, Matrix.Up, Length, Thickness, blendType: BlendType);
-            MyTransparentGeometry.AddLineBillboard(MaterialSquare, Color.Blue, Matrix.Translation, Matrix.Backward, Length, Thickness, blendType: BlendType);
+            MatrixD wm = Matrix;
+            float thick = Thickness;
+            float len = Length;
+            Render(wm, len, thick);
 
-            float negativeAxisOpacity = 0.2f;
-            MyTransparentGeometry.AddLineBillboard(MaterialSquare, Color.Red * negativeAxisOpacity, Matrix.Translation, Matrix.Left, Length, Thickness, blendType: BlendType);
-            MyTransparentGeometry.AddLineBillboard(MaterialSquare, Color.Green * negativeAxisOpacity, Matrix.Translation, Matrix.Down, Length, Thickness, blendType: BlendType);
-            MyTransparentGeometry.AddLineBillboard(MaterialSquare, Color.Blue * negativeAxisOpacity, Matrix.Translation, Matrix.Forward, Length, Thickness, blendType: BlendType);
+            if(DrawOnTop)
+            {
+                float depthScale = ToAlwaysOnTop(ref wm);
+                thick *= depthScale;
+                // len is already affected by matrix scale, no need to be rescaled
+                Render(wm, len, thick, OnTopColorMul);
+            }
+        }
+
+        static void Render(MatrixD wm, float len, float thick, float colorMul = 1f)
+        {
+            MyTransparentGeometry.AddLineBillboard(MaterialSquare, Color.Red * colorMul, wm.Translation, wm.Right, len, thick, blendType: BlendType);
+            MyTransparentGeometry.AddLineBillboard(MaterialSquare, Color.Green * colorMul, wm.Translation, wm.Up, len, thick, blendType: BlendType);
+            MyTransparentGeometry.AddLineBillboard(MaterialSquare, Color.Blue * colorMul, wm.Translation, wm.Backward, len, thick, blendType: BlendType);
+
+            float negativeAxisOpacity = 0.2f * colorMul;
+            MyTransparentGeometry.AddLineBillboard(MaterialSquare, Color.Red * negativeAxisOpacity, wm.Translation, wm.Left, len, thick, blendType: BlendType);
+            MyTransparentGeometry.AddLineBillboard(MaterialSquare, Color.Green * negativeAxisOpacity, wm.Translation, wm.Down, len, thick, blendType: BlendType);
+            MyTransparentGeometry.AddLineBillboard(MaterialSquare, Color.Blue * negativeAxisOpacity, wm.Translation, wm.Forward, len, thick, blendType: BlendType);
         }
 
         public override void Dispose()
